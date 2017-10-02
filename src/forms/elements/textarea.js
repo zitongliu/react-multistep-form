184090125884e6b51a6501da7c986d0408b8c724
zitongliu <zitongliu7@gmail.com>
2017-10-02 12:14:25 +1100

    created textarea form element

---
+++ src/forms/elements/textarea.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TextareaField = (props) => {
+
+  return (
+    <div className="ctrl-holder">
+      <label htmlFor={props.id}>{props.label}</label>
+      <div className="ctrl">
+        <input type="textarea" id={props.id} name={props.name} placeholder={props.placeholder} onChange={(event)=> { props.handleInputChange(props.name, event.target.value ) }} />
+      </div>
+    </div>
+  );
+}
+
+export default TextareaField;
